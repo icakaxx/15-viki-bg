26d30e34b399a53a06a0ef78cce572d478bfc4bc
icakaxx <aphtex@gmail.com>
2025-07-17 22:50:20 +0300

    fix 2

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   i18n: {
     defaultLocale: 'bg',
     locales: ['bg', 'en'],
-    localeDetection: true,
+    localeDetection: false,
   },
   localePath: typeof window === 'undefined' ? require('path').resolve('./public/locales') : '/locales',
   reloadOnPrerender: process.env.NODE_ENV === 'development',
